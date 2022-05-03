4052441e686b3775d1720b8cfa0e6e702a346eeb
Xushnora <xushnoranishonova@gmail.com>
2022-05-03 11:56:54 -0700

    changed checked

--- js/index.js
+++ js/index.js
@@ -1,20 +1,34 @@
+
 const elList = document.querySelector('.list');
-let elErrorBox = document.querySelector('.error');
+const elErrorBox = document.querySelector('.error');
 let resCount = document.querySelector('.resCount');
 // restart timer
 let elgameModalBox = document.querySelector('.gameModal');
 let elRestartBtn = document.querySelector('.btnRestart');
 
-// ****
 let generatedArr = randomArr(pokemons);
 let clickCount = 0;
+let couter = 0;
 let selectElement = null;
-let counter = 0;
-const myColumns = {
-  y1: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  y6: [50, 51, 52, 53, 54, 55, 56, 57, 58, 59],
+let columns = {
   x1: [0, 10, 20, 30, 40, 50],
+  x2: [1, 11, 21, 31, 41, 51],
+  x3: [2, 12, 22, 32, 42, 52],
+  x4: [3, 13, 23, 33, 43, 53],
+  x5: [4, 14, 24, 34, 44, 54],
+  x6: [5, 15, 25, 35, 45, 55],
+  x7: [6, 16, 26, 36, 46, 56],
+  x8: [7, 17, 27, 37, 47, 57],
+  x9: [8, 18, 28, 38, 48, 58],
   x10: [9, 19, 29, 39, 49, 59],
+};
+let rows = {
+  y1: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  y2: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+  y3: [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+  y4: [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
+  y5: [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
+  y6: [50, 51, 52, 53, 54, 55, 56, 57, 58, 59],
 };
 
 // Create random array
@@ -22,8 +36,8 @@ function randomArr(arr) {
   let newRandomArray = Array(4).fill(arr).flat(1);
 
   for (let i = 0; i < 150; i++) {
-    let index = parseInt(Math.random() * 60);
-    let rendomItem = newRandomArray.splice(index, 1)[0];
+    var index = parseInt(Math.random() * 60);
+    var rendomItem = newRandomArray.splice(index, 1)[0];
 
     if (rendomItem) {
       newRandomArray = [...newRandomArray, rendomItem];
@@ -66,21 +80,248 @@ function alertErrorBox() {
   elErrorBox.style.color = "red" 
 }
 
-// For counter ---
-// function counterBox(counter = 1) {
-//   counter++;
-//   resCount.textContent = `+ ${counter}`
-// }
+// Found column name
+function foundColumn(index) {
+  for (const int in columns) {
+    for (let i = 0; i < columns[int].length; i++) {
+      if (columns[int][i] === index) {
+        return int;
+      }
+    }
+  }
+}
+
+// Found row name
+function foundRow(index) {
+  for (const int in rows) {
+    for(let i = 0; i < rows[int].length; i++) {
+      if(rows[int][i] === index) {
+        return int
+      }
+    }
+  }
+}
+
+// Delete element in column
+function deleteElementColumn(columnName, index) {
+  for (let i = 0; i < columns[columnName].length; i++) {
+    if (columns[columnName][i] === index) {
+      columns[columnName][i] = null;
+    }
+  }
+}
+
+// Delete element in row
+function deleteElementRow(rowName, index) {
+  for (let i = 0; i < rows[rowName].length; i++) {
+    if (rows[rowName][i] === index) {
+      rows[rowName][i] = null;
+    }
+  }
+}
 
 // Delete elements
-function deleteElements(firstIndex, secondIndex) {
-  generatedArr[firstIndex] = null;
+function deleteElements(fistIndex, secondIndex) {
+  generatedArr[fistIndex] = null;
   generatedArr[secondIndex] = null;
 
+  deleteElementColumn(foundColumn(fistIndex), fistIndex);
+  deleteElementColumn(foundColumn(secondIndex), secondIndex);
+  deleteElementRow(foundRow(fistIndex), fistIndex);
+  deleteElementRow(foundRow(secondIndex), secondIndex);
   renderElements(generatedArr);
 }
 
-// Check delete
+// Delete element in BUG
+function checkForDeleteInner(firstIndex, secondIndex) {
+  let high = firstIndex > secondIndex ? firstIndex : secondIndex;
+  let low = firstIndex > secondIndex ? secondIndex : firstIndex;
+  
+  if(high - low == 9) {
+    if(columns.x1[0] == null || columns.x2[1] == null){
+      deleteElements(firstIndex, secondIndex);
+      alertSuccessBox()
+    }
+  } else if(high - low == 9 && columns.x2[0] == null || columns.x3[1] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x3[0] == null || columns.x4[1] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x4[0] == null || columns.x5[1] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x5[0] == null || columns.x6[1] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x6[0] == null || columns.x7[1] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x7[0] == null || columns.x8[1] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x8[0] == null || columns.x9[1] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x9[0] == null || columns.x10[1] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } 
+  // two block
+   else if(high - low == 9 && columns.x1[1] == null || columns.x2[2] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x2[1] == null || columns.x3[2] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x3[1] == null || columns.x4[2] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x4[1] == null || columns.x5[2] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x5[1] == null || columns.x6[2] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x6[1] == null || columns.x7[2] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x7[1] == null || columns.x8[2] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x8[1] == null || columns.x9[2] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x9[1] == null || columns.x10[2] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }
+  // three block
+  else if(high - low == 9 && columns.x1[2] == null || columns.x2[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x1[2] == null || columns.x2[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x1[2] == null || columns.x2[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x1[2] == null || columns.x2[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x1[2] == null || columns.x2[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x2[2] == null || columns.x3[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x3[2] == null || columns.x4[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x4[2] == null || columns.x5[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x5[2] == null || columns.x6[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x6[2] == null || columns.x7[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x7[2] == null || columns.x8[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x8[2] == null || columns.x9[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x9[2] == null || columns.x10[3] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } 
+  // // four block
+  else if(high - low == 9 && columns.x1[3] == null || columns.x2[4] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x2[3] == null || columns.x3[4] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x3[3] == null || columns.x4[4] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x4[3] == null || columns.x5[4] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x5[3] == null || columns.x6[4] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x6[3] == null || columns.x7[4] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x7[3] == null || columns.x8[4] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x8[3] == null || columns.x9[4] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x9[3] == null || columns.x10[4] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } 
+  // // five block
+  else if(high - low == 9 && columns.x1[4] == null || columns.x2[5] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x2[4] == null || columns.x3[5] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x3[4] == null || columns.x4[5] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x4[4] == null || columns.x5[5] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x5[4] == null || columns.x6[5] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else if(high - low == 9 && columns.x6[4] == null || columns.x7[5] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x7[4] == null || columns.x8[5] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x8[4] == null || columns.x9[5] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  }  else if(high - low == 9 && columns.x9[4] == null || columns.x10[5] == null) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+  } else {
+    alertErrorBox()
+    renderElements(generatedArr);
+  }
+
+}
+
+// check elements 
+function checkDeleteOther(bigIndex, smallIndex) {
+  let highRow = foundRow(bigIndex);
+  let lowRow = foundRow(smallIndex);
+
+  if(
+    bigIndex - smallIndex < 10 &&
+    highRow.split('')[1] - lowRow.split('')[1] === 1
+  ) {
+    if(!rows[lowRow][bigIndex - 10]) {
+      deleteElements(bigIndex, smallIndex);
+      alertSuccessBox()
+      resCount.textContent = `${++couter} +`;
+    }
+  } else {
+    alertErrorBox()
+    renderElements(generatedArr);
+  }
+}
+
+// Check elements
 function checkForDelete(firstIndex, secondIndex) {
   let high = firstIndex > secondIndex ? firstIndex : secondIndex;
   let low = firstIndex > secondIndex ? secondIndex : firstIndex;
@@ -88,37 +329,39 @@ function checkForDelete(firstIndex, secondIndex) {
   if (high - low == 1 || high - low == 10) {
     deleteElements(firstIndex, secondIndex);
     alertSuccessBox()
+    resCount.textContent = `${++couter} +`;
   } else if (
-    myColumns.y1.includes(firstIndex) &&
-    myColumns.y1.includes(secondIndex)
-  ) {
+    rows.y1.includes(firstIndex) &&
+    rows.y1.includes(secondIndex)) {
     deleteElements(firstIndex, secondIndex);
-    alertSuccessBox()
+    alertSuccessBox();
+    resCount.textContent = `${++couter} +`;
   } else if (
-    myColumns.y6.includes(firstIndex) &&
-    myColumns.y6.includes(secondIndex)
-  ) {
+    rows.y6.includes(firstIndex) &&
+    rows.y6.includes(secondIndex)) {
     deleteElements(firstIndex, secondIndex);
-    alertSuccessBox()
+    alertSuccessBox();
+    resCount.textContent = `${++couter} +`;
   } else if (
-    myColumns.x1.includes(firstIndex) &&
-    myColumns.x1.includes(secondIndex)
-  ) {
-    deleteElements(firstIndex, secondIndex);
-    alertSuccessBox()
-  } else if (
-    myColumns.x10.includes(firstIndex) &&
-    myColumns.x10.includes(secondIndex)
+    columns.x1.includes(firstIndex) &&
+    columns.x1.includes(secondIndex)
   ) {
     deleteElements(firstIndex, secondIndex);
     alertSuccessBox();
+    resCount.textContent = `${++couter} +`;
+  } else if (
+    columns.x10.includes(firstIndex) &&
+    columns.x10.includes(secondIndex)
+  ) {
+    deleteElements(firstIndex, secondIndex);
+    alertSuccessBox()
+    resCount.textContent = `${++couter} +`;
   } else {
-    alertErrorBox()
-    renderElements(generatedArr);
+    checkDeleteOther(high, low)
   }
 }
 
-// Handle click event
+// Click event
 function handleClick(evt) {
   if (evt.target.matches('.poke-img')) {
     evt.target.closest('.cardItem').classList.add('cardItem-active');
@@ -136,9 +379,7 @@ function handleClick(evt) {
       clickCount = 0;
 
       if (selectElement.id === id && selectElement.index !== index) {
-        setTimeout(function(){
-          checkForDelete(index - 0, selectElement.index - 0);
-        }, 300);
+        checkForDelete(index - 0, selectElement.index - 0);
       } else {
         alertErrorBox()
         renderElements(generatedArr);
@@ -184,3 +425,4 @@ function timer() {
 }
 
 timer();
+
